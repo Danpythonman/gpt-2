a559dc0864f6184c957494f2ce146cc03aaf7547
Daniel Di Giovanni <dannyjdigio@gmail.com>
2025-07-03 14:36:11 -0400

    Gradient accumulation over large batch size

--- train.py
+++ train.py
@@ -25,15 +25,30 @@ print(f'Using device: {device}')
 
 torch.set_float32_matmul_precision('high')
 
-batch_size = 8
 block_size = 1024
 vocab_size=50_304
 n_layer = 12
 n_head = 12
 n_embd = 768
 
+total_batch_size = 2**19 # Approximately 0.5 million, as in the GPT-3 paper
+mini_batches = 8
+mini_batch_size = mini_batches * block_size
+
+if total_batch_size % mini_batch_size != 0:
+    raise Exception('Total batch size is not divisible by mini batch size')
+
+grad_accum_steps = int(total_batch_size / mini_batch_size)
+
+print(
+    f'Total desired batch size is {total_batch_size:,} tokens, mini batch size '
+    f'is {mini_batch_size:,} tokens ({mini_batches} examples of length '
+    f'{block_size}), so gradients need to be accumulated for '
+    f'{grad_accum_steps} mini batches per batch'
+)
+
 data_loader = DataLoader(
-    batch_size=batch_size,
+    batch_size=mini_batches,
     block_size=block_size,
     filepath=Path(__file__).parent / 'tiny-shakespeare.txt',
     device=device
@@ -48,10 +63,15 @@ model: GPT = GPT(
     flash_attention=True
 )
 
+print('Moving model to GPU')
 model.to(device)
+
+print('Compiling model')
 model = torch.compile(model)
 
 max_steps = 50
+
+print('Configuring optimizer and learning rate scheduler')
 optimizer = model.configure_optimizer(
     weight_decay=0.1,
     learning_rate=6e-4,
@@ -66,14 +86,23 @@ scheduler = GPT2CosineLearningRateScheduler(
     max_steps=max_steps
 )
 
+print(f'Training for {max_steps} iterations')
+
 for step, (x, y) in enumerate(data_loader):
     if step == max_steps: break
+
     optimizer.zero_grad()
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        logits = model(x)
-        loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), y.view(-1))
-    loss.backward()
+    accumulated_loss = 0.0
+
+    for micro_step in range(grad_accum_steps):
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits = model(x)
+            loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), y.view(-1))
+        loss /= grad_accum_steps
+        accumulated_loss += loss.detach()
+        loss.backward()
+
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
     lr = scheduler.step(step)
     optimizer.step()
-    print(f'step: {step:>3} | loss: {loss.item():8>.4f} | lr: {lr:8>.4e} | norm: {norm.item():8>.4f}')
+    print(f'step: {step:>3} | loss: {accumulated_loss.item():9>.4f} | lr: {lr:8>.4e} | norm: {norm.item():8>.4f}')
